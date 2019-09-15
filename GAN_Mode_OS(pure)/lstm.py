d35bfe628900beb1f0b02aba8d6af364459f2edd
최재원 <dnjsdlghdi@gmail.com>
2019-09-15 13:50:47 +0900

    os(pure)model

---
+++ GAN_Mode_OS(pure)/lstm.py
@@ -0,0 +1,146 @@
+'''
+----------------------------코드 설명----------------------------
+-C-
+3.LSTM에 해당하는 코드로
+LSTM으로 구현함
+----------------------------고려 사항----------------------------
+lstm은 input x에 이미 exogenous가 포함되어있다.
+하지만 conv_lstm구현시 따로 받아야함으로 preprocess를 수정해야 한다
+수정사항
+1. exogenous도 lstm버전이 있어야한다.(즉 CELL_SIZE 따라 반복 되어야함)
+2. 기존의 lstm버전은 speed만 고려해야한다.(vector를 66개 -> 12개로 수정)
+'''
+from module import *
+import os
+#LSTM을 구현
+def model(S, Y):
+    layer = LSTM_model(S)
+
+    cost_MAE = MAE(Y, layer)
+    cost_MSE = MSE(Y, layer)
+    cost_MAPE = MAPE(Y, layer)
+    optimal = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(cost_MSE)
+
+    return cost_MAE, cost_MSE, cost_MAPE, optimal
+
+#training 해준다.
+def train(S_data, Y_data, cost_MAE, cost_MSE, cost_MAPE, cost_MAE_hist, cost_MSE_hist, cost_MAPE_hist, optimal, train_idx, test_idx, cr_idx, writer_train, writer_test, train_result, test_result, CURRENT_POINT_DIR, start_from):
+    BATCH_NUM = int(len(train_idx) / BATCH_SIZE)
+    print('BATCH_NUM: %d' % BATCH_NUM)
+    for _ in range(start_from):
+        np.random.shuffle(train_idx)
+    global_step_tr = 0
+    global_step_te = 0
+    for tr_idx in range(start_from, TRAIN_NUM):
+        epoch_mse_cost = 0.0
+        epoch_mape_cost = 0.0
+        for ba_idx in range(BATCH_NUM):
+            #Batch Slice
+            S_train = batch_slice(S_data, train_idx, ba_idx, 'LSTM', 1)
+            Y_train = batch_slice(Y_data, train_idx, ba_idx, 'LSTMY', 1)
+
+            cost_MSE_val, cost_MAPE_val, cost_MSE_hist_val, _= sess.run([cost_MSE, cost_MAPE, cost_MSE_hist, optimal], feed_dict={S:S_train, Y: Y_train })
+            epoch_mse_cost += cost_MSE_val
+            epoch_mape_cost += cost_MAPE_val
+            writer_train.add_summary(cost_MSE_hist_val, global_step_tr)
+            global_step_tr += 1
+
+        # 설정 interval당 train과 test 값을 출력해준다.
+        if tr_idx % TRAIN_PRINT_INTERVAL == 0:
+            train_result.append([epoch_mse_cost / BATCH_NUM, epoch_mape_cost / BATCH_NUM])
+            print("Train Cost %d: %lf %lf" % (tr_idx, epoch_mse_cost / BATCH_NUM, epoch_mape_cost / BATCH_NUM))
+        if (tr_idx+1) % TEST_PRINT_INTERVAL == 0:
+
+            print("Saving network...")
+            sess.run(last_epoch.assign(tr_idx + 1))
+            if not os.path.exists(CURRENT_POINT_DIR):
+                os.makedirs(CURRENT_POINT_DIR)
+            saver.save(sess, CURRENT_POINT_DIR + "/model", global_step=tr_idx, write_meta_graph=False)
+
+            global_step_te=test(S_data, Y_data, cost_MAE, cost_MSE, cost_MAPE, cost_MAE_hist, cost_MSE_hist, cost_MAPE_hist, test_idx, tr_idx, global_step_te, cr_idx, writer_test, test_result)
+
+        #cross validation의 train_idx를 shuffle해준다.
+        np.random.shuffle(train_idx)
+
+
+#testing 해준다.
+def test(S_data, Y_data, cost_MAE, cost_MSE, cost_MAPE, cost_MAE_hist, cost_MSE_hist, cost_MAPE_hist, test_idx, tr_idx, global_step_te, cr_idx, writer_test, test_result):
+    BATCH_NUM = int(len(test_idx))
+    # Batch Slice
+    S_test = batch_slice(S_data, test_idx, 0, 'LSTM', 1, TEST_BATCH_SIZE)
+    Y_test = batch_slice(Y_data, test_idx, 0, 'LSTMY', 1, TEST_BATCH_SIZE)
+
+    mae, mse, mape, cost_MAE_hist_val, cost_MSE_hist_val, cost_MAPE_hist_val = sess.run([cost_MAE, cost_MSE, cost_MAPE, cost_MAE_hist, cost_MSE_hist, cost_MAPE_hist], feed_dict={S:S_test, Y:Y_test})
+
+    writer_test.add_summary(cost_MAE_hist_val, global_step_te)
+    writer_test.add_summary(cost_MSE_hist_val, global_step_te)
+    writer_test.add_summary(cost_MAPE_hist_val, global_step_te)
+
+    global_step_te += 1
+
+    test_result.append([mae, mse, mape])
+    final_result[cr_idx].append(mape)
+    print("Test Cost(%d) %d: MAE(%lf) MSE(%lf) MAPE(%lf)" % (cr_idx, tr_idx, mae, mse, mape))
+    return global_step_te
+
+
+
+
+###################################################-MAIN-###################################################
+S_data, _, _,Y_data= input_data(0b101)
+final_result = [[] for i in range(CROSS_ITERATION_NUM)]
+
+cr_idx = 0
+for train_idx, test_idx in load_Data():
+    print('CROSS VALIDATION: %d' % cr_idx)
+
+    train_result = []
+    test_result = []
+
+    S = tf.placeholder("float32", [CELL_SIZE, None, TIME_STAMP]) #cell_size, batch_size
+    Y = tf.placeholder("float32", [None, 1])
+    last_epoch = tf.Variable(0, name=LAST_EPOCH_NAME)
+
+    init()
+    sess = tf.Session()
+    cost_MAE, cost_MSE, cost_MAPE, optimal = model(S, Y)
+    writer_train = tf.summary.FileWriter("./tensorboard/lstm/train%d" % cr_idx, sess.graph)
+    writer_test = tf.summary.FileWriter("./tensorboard/lstm/test%d" % cr_idx, sess.graph)
+    cost_MAE_hist = tf.summary.scalar('cost_MAE', cost_MAE)
+    cost_MSE_hist = tf.summary.scalar('cost_MSE', cost_MSE)
+    cost_MAPE_hist = tf.summary.scalar('cost_MAPE', cost_MAPE)
+    sess.run(tf.global_variables_initializer())
+
+    # Saver and Restore
+    saver = tf.train.Saver()
+    CURRENT_POINT_DIR = CHECK_POINT_DIR + "LSTM_" + str(cr_idx) + "/"
+
+    if RESTORE_FLAG:
+        checkpoint = tf.train.get_checkpoint_state(CURRENT_POINT_DIR)
+
+        if checkpoint and checkpoint.model_checkpoint_path:
+            try:
+                saver.restore(sess, checkpoint.model_checkpoint_path)
+                print("Successfully loaded:", checkpoint.model_checkpoint_path)
+            except:
+                print("Error on loading old network weights")
+        else:
+            print("Could not find old network weights")
+
+    start_from = sess.run(last_epoch)
+    # train my model
+    print('Start learning from:', start_from)
+
+    train(S_data, Y_data, cost_MAE, cost_MSE, cost_MAPE, cost_MAE_hist, cost_MSE_hist, cost_MAPE_hist, optimal, train_idx, test_idx, cr_idx, writer_train, writer_test, train_result, test_result,  CURRENT_POINT_DIR, start_from)
+
+    tf.reset_default_graph()
+
+    output_data(train_result, test_result, 'lstm', cr_idx)
+
+    cr_idx = cr_idx + 1
+
+    if(cr_idx == CROSS_ITERATION_NUM):
+        break
+
+
+output_result(final_result, 'lstm' + "_", cr_idx)
