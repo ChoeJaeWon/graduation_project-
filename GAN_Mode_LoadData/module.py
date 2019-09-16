585ea1e74fcff24669d8da4f04ce093afd8d2be6
재원 <dnjsdlghdi@naver.com>
2019-09-17 00:19:48 +0900

    USE_LOAD

--- GAN_Mode_LoadData/module.py
+++ GAN_Mode_LoadData/module.py
@@ -61,9 +61,10 @@ PHASE1_EPOCH = 10
 PHASE2_EPOCH = 20
 
 #FLAG
-RESTORE_FLAG = True #weight 불러오기 여부 [default False]
-RESTORE_GENERATOR_FLAG = True #Generator weight 불러오기 여부 [RESTORE_FLAG]가 False 이면 항상 False[default False]
-LATENT_VECTOR_FLAG = False #generator가 12짜리 vector를 생산할 것인가 또는 scalar 예측값을 생산할 것인가
+USE_LOAD = False
+RESTORE_FLAG = USE_LOAD #weight 불러오기 여부 [default False]
+RESTORE_GENERATOR_FLAG = USE_LOAD #Generator weight 불러오기 여부 [RESTORE_FLAG]가 False 이면 항상 False[default False]
+LATENT_VECTOR_FLAG = USE_LOAD #generator가 12짜리 vector를 생산할 것인가 또는 scalar 예측값을 생산할 것인가
 MASTER_SAVE_FLAG = False #[WARNING] 저장이 되지 않습니다. (adv 모델에 한해 적용)
 
 #Fix value(Week Cross Validation)
