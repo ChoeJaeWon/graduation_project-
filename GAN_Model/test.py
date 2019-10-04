3c4f51ddc2866713ce8bd0e394abe02796d40086
최재원 <dnjsdlghdi@gmail.com>
2019-10-04 14:04:37 +0900

    concat

--- GAN_Model/test.py
+++ GAN_Model/test.py
@@ -2,18 +2,11 @@ import numpy as np
 from sklearn.model_selection import KFold
 import tensorflow as tf
 
-np.random.seed(77)
+a = []
 
-X = np.array([1, 2,3, 4])
-Y = np.array([2, 3])
+a.append([1,2])
+a.append([1,2])
 
-kf = KFold(n_splits=4, shuffle=True)
-for train_index, test_index in kf.split(X):
-    for _ in range(4):
-        print("TRAIN:", train_index, "TEST:", test_index)
-        np.random.shuffle(train_index)
+print(a)
 
-
-
-
-#print(0b1111& 0b1000)
+print(8%5)
